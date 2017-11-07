da69b922e076a58cd471640f4afd6fe7bcc5e0b9
Matt Putipong <mattputipong@gmail.com>
2017-11-06 19:55:57 -0500

    - display NFL stadiums around the country on a Leaflet.js map - apply tooltips on markers to display properties about each stadium - display a table of all stadiums with their corresponding teams

---
+++ imports/js/main.js
@@ -0,0 +1,78 @@
+/**
+ * Created by mattputipong on 11/6/17.
+ */
+
+'use strict';
+
+$( document ).ready( function() {
+	let mapper = new Mapper();
+
+	mapper.getJson( 'https://raw.github.com/sisskind/GeoJSON-Football/master/stadiums.geojson' ).then( json => {
+		mapper.addMarkers( json );
+	} );
+} );
+
+class Mapper {
+	constructor() {
+		this.osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+		this.osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+		this.osm = L.tileLayer( this.osmUrl, {
+			maxZoom     : 18,
+			attribution : this.osmAttrib
+		} );
+		this.map = L.map( 'map' ).setView( [ 40, -100 ], 4 ).addLayer( this.osm );
+	}
+
+	getJson( file ) {
+		return new Promise( ( res, rej ) => {
+			$.get( file )
+				.done( resp => res( JSON.parse( resp ) ) )
+				.fail( err => rej( err ) );
+		} );
+	}
+
+	addMarkers( data ) {
+		let allStadiums = L.geoJson( data, {
+			pointToLayer: ( feat, latlng ) => {
+				let html = this.markerHtml( feat.properties );
+
+				return L.marker( latlng ).on( 'mouseover', function() {
+					this.bindPopup( html ).openPopup();
+				} );
+			},
+			onEachFeature: feat => {
+				let html = this.tableHtml( feat.properties );
+				$( '#nfl-table tbody' ).append( html );
+			}
+		} );
+
+		allStadiums.addTo( this.map );
+	}
+
+	markerHtml( data ) {
+		return $([
+			"<table class='table table-bordered'>",
+			"   <tbody>",
+			"       <tr>",
+			"           <th>Stadium</th>",
+			`           <td>${data.Stadium}</td>`,
+			"       </tr>",
+			"       <tr>",
+			"           <th>Team</th>",
+			`           <td>${data.Team}</td>`,
+			"       </tr>",
+			"   </tbody>",
+			"</table>"
+		].join('\n')).get( 0 );
+	}
+
+	tableHtml( data ) {
+		return $([
+			"<tr>",
+			`   <td>${data.Stadium}</td>`,
+			`   <td>${data.Team}</td>`,
+			`   <td>${data.League}</td>`,
+			"</tr>"
+		].join('\n')).get( 0 );
+	}
+}
