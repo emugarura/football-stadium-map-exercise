1b3b741549de1050bbeba006dce38643abbeb2c4
Matt Putipong <mattputipong@gmail.com>
2017-11-07 21:00:03 -0500

    - `index.js` used as entry point for webpack

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,15 @@
 /**
  * Created by mattputipong on 11/7/17.
  */
+
+'use strict';
+
+import Mapper from './Mapper';
+
+$( document ).ready( function() {
+    let mapper = new Mapper();
+
+    mapper.getJson( 'https://s3.amazonaws.com/mputipong/football-map-exercise/stadiums.geojson' )
+        .then( json => mapper.addMarkers( json ) )
+        .catch( err => console.log( err ) );
+} );
